05788d17dbedcbe5d5e68f0cddaf62d466408b6b
MiguelEnrique94 <mienrique94@gmail.com>
2023-10-16 22:28:57 -0300

    adicionado rotas

--- Astronomy/AppShell.xaml.cs
+++ Astronomy/AppShell.xaml.cs
@@ -1,9 +1,12 @@
-﻿namespace Astronomy;
+﻿using Astronomy.Pages;
+
+namespace Astronomy;
 
 public partial class AppShell : Shell
 {
     public AppShell()
     {
         InitializeComponent();
+        Routing.RegisterRoute("astronomicalbodydetails", typeof(AstronomicalBodyPage));
     }
 }
